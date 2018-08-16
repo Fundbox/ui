f2659ee5c32969f5c69ee4af303a56db8013e61e
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2018-08-16 10:13:36 +0300

    Added missing node packages and updated some configs

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,8 @@ import { configure } from '@storybook/vue'
 import { setOptions } from '@storybook/addon-options';
 
 setOptions({
-  name: 'Funbox Components Library',
-  url: 'https://github.com/Fundbox/frontend',
+  name: 'Fundbox UI Components Library',
+  url: 'https://github.com/Fundbox/ui',
   addonPanelInRight: true,
 });
 

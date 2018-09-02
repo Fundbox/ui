7a8d0b25e3bd2bc09fecb4066011b0f891d2c1d1
forforeach <dima.kuzmich.work@gmail.com>
2018-09-02 09:52:15 +0300

    feat(stories): add checkbox stories for events and validations

--- components/checkbox/checkbox.stories.js
+++ components/checkbox/checkbox.stories.js
@@ -1,19 +1,34 @@
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from 'storybook-addon-vue-info';
-import { text, boolean } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 
 import FbxCheckbox from './Checkbox';
 import summary from './checkbox.md';
 
 const stories = storiesOf('Checkbox', module);
 
-stories.add('with text', withInfo({ summary })(() => ({
+stories.add('simple', withInfo({ summary })(() => ({
   components: { FbxCheckbox },
   template: `<fbx-checkbox
                 name="foo"
                 v-model="value"
-              >${text('Text', 'Hello checkbox')}</fbx-checkbox>`,
+              >${text('Text', 'Simple checkbox usage')}</fbx-checkbox>`,
+  data () { return { value: true } },
+  watch: {
+    value (val) {
+      action(`New value: ${val}`)()
+    },
+  }
+})));
+
+stories.add('with validation', withInfo({ summary })(() => ({
+  components: { FbxCheckbox },
+  template: `<fbx-checkbox
+                name="foo"
+                v-model="value"
+                validations="required"
+              >${text('Text', 'Checkbox with required validation')}</fbx-checkbox>`,
   data () { return { value: true } },
   watch: {
     value: function (val) {
@@ -22,8 +37,17 @@ stories.add('with text', withInfo({ summary })(() => ({
   }
 })));
 
-stories.add('with some emoji', () => ({
+stories.add('with events', withInfo({ summary })(() => ({
   components: { FbxCheckbox },
-  template: '<fbx-checkbox @click="click">😀 😎 👍 💯</fbx-checkbox>',
-  methods: { click: action('clicked') },
-}));
+  template: `<fbx-checkbox
+                name="foo"
+                v-model="value"
+                @click="onClick"
+                @change="onChange"
+              >${text('Text', 'Checkbox with event handlers')}</fbx-checkbox>`,
+  data () { return { value: true } },
+  methods: {
+    onClick: action('Checkbox click handler'),
+    onChange: action('Checkbox change handler'),
+  }
+})));

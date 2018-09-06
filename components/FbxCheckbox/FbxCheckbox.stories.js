8adb68daca5c1b8549f8e086fe626e5fc5b9f575
Nicholas Litwin <nick.litwin@fundbox.com>
2018-09-06 09:40:40 -0700

    Move checkbox to new folder/names

---
+++ components/FbxCheckbox/FbxCheckbox.stories.js
@@ -0,0 +1,53 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { withInfo } from 'storybook-addon-vue-info';
+import { text } from '@storybook/addon-knobs';
+
+import FbxCheckbox from './FbxCheckbox.vue';
+import summary from './FbxCheckbox.md';
+
+const stories = storiesOf('Checkbox', module);
+
+stories.add('simple', withInfo({ summary })(() => ({
+  components: { FbxCheckbox },
+  template: `<fbx-checkbox
+                name="foo"
+                v-model="value"
+              >${text('Text', 'Simple checkbox usage')}</fbx-checkbox>`,
+  data () { return { value: true } },
+  watch: {
+    value (val) {
+      action(`New value: ${val}`)()
+    },
+  }
+})));
+
+stories.add('with validation', withInfo({ summary })(() => ({
+  components: { FbxCheckbox },
+  template: `<fbx-checkbox
+                name="foo"
+                v-model="value"
+                validations="required"
+              >${text('Text', 'Checkbox with required validation')}</fbx-checkbox>`,
+  data () { return { value: true } },
+  watch: {
+    value: function (val) {
+      action(`New value: ${val}`)()
+    },
+  }
+})));
+
+stories.add('with events', withInfo({ summary })(() => ({
+  components: { FbxCheckbox },
+  template: `<fbx-checkbox
+                name="foo"
+                v-model="value"
+                @click="onClick"
+                @change="onChange"
+              >${text('Text', 'Checkbox with event handlers')}</fbx-checkbox>`,
+  data () { return { value: true } },
+  methods: {
+    onClick: action('Checkbox click handler'),
+    onChange: action('Checkbox change handler'),
+  }
+})));
--- components/main.stories.js
+++ components/main.stories.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
 
-import IntroductionStories from './introduction/introduction.stories';
-import FbxButtonStories from './FbxButton/FbxButton.stories';
+import IntroductionStories from './introduction/introduction.stories.js';
+import FbxButtonStories from './FbxButton/FbxButton.stories.js';
+import FbxCheckboxStories from './FbxCheckbox/FbxCheckbox.stories.js';

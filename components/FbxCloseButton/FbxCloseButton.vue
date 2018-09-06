3a99692cff42238fe9558e00698f69358ecd1c05
Nicholas Litwin <nick.litwin@fundbox.com>
2018-09-06 09:43:01 -0700

    Add FbxCloseButton

---
+++ components/FbxCloseButton/FbxCloseButton.vue
@@ -0,0 +1,47 @@
+<template>
+<span
+  class="fbx-close-button"
+  :class="{ 'fbx-icon-close_hover': hover, 'fbx-icon-close': !hover }"
+  @mouseover="toggleHover"
+  @mouseleave="toggleHover"
+  v-on="listeners"></span>
+
+</template>
+
+<script>
+export default {
+  name: "fbx-close-button",
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+      }
+    },
+  },
+  methods: {
+    toggleHover() {
+      this.hover = !this.hover;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~fbx_common/src/styles/utils/color-palette.scss";
+@import "~fbx_common/src/styles/fonts/icomoon/style.css";
+
+.fbx-close-button {
+  // Make sure icon container is not bigger then the icon itself.
+  // It must be `important!` because combined with utility class
+  // there can be an unwanted line-height override.
+  display: block;
+  line-height: 0 !important;
+  color: $dark-green;
+  cursor: pointer;
+}
+</style>

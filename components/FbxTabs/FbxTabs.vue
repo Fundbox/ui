cfcb17ef0dc626b4c2aa59f26d1591d7b1f7c0ef
Nicholas Litwin <nick.litwin@fundbox.com>
2018-09-19 23:29:34 -0700

    Move .fbx-tab out one level

--- components/FbxTabs/FbxTabs.vue
+++ components/FbxTabs/FbxTabs.vue
@@ -39,26 +39,26 @@ export default {
 
 .fbx-tabs {
   display: flex;
+}
 
-  .fbx-tab {
-    padding: 14px 30px;
-    @include font(16);
-    color: $medium-blue;
-    background-color: $light-gray;
-    border: 1px solid $medium-gray;
-    text-align: center;
-    cursor: pointer;
+.fbx-tab {
+  padding: 14px 30px;
+  @include font(16);
+  color: $medium-blue;
+  background-color: $light-gray;
+  border: 1px solid $medium-gray;
+  text-align: center;
+  cursor: pointer;
 
-    & + .fbx-tab {
-      border-left: none;
-    }
+  & + .fbx-tab {
+    border-left: none;
+  }
 
-    &.active {
-      font-weight: 500;
-      background-color: $white;
-      border-bottom: 1px solid $white;
-      cursor: default;
-    }
+  &.active {
+    font-weight: 500;
+    background-color: $white;
+    border-bottom: 1px solid $white;
+    cursor: default;
   }
 }
 </style>

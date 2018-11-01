8230c1a684fe395938c3f48259b8ccd35f8e69b7
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-01 13:43:08 -0700

    Add basic mobile styles and layout

--- components/FbxPanelRadioButton/FbxPanelRadioButton.vue
+++ components/FbxPanelRadioButton/FbxPanelRadioButton.vue
@@ -8,8 +8,17 @@
     class="radio-input--hidden"
     v-bind="$attrs"
   />
-  <div class="fbx-panel-radio-button__inner-content">
-    <slot></slot>
+  <div class="panel-radio-button__inner-content">
+    <div class="panel-radio-button__header">
+      <div class="panel-radio-button__circle-indicator">
+        <div class="selected-radio-circle"></div>
+      </div>
+
+    </div>
+
+    <div class="panel-radio-button__body">
+      <slot name="body"></slot>
+    </div>
   </div>
 </label>
 </template>
@@ -22,15 +31,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.fbx-panel-radio-button {
-  .radio-input--hidden {
-    position: absolute;
-    opacity: 0;
-  }
+@import "../styles/utils/mixins.scss";
+@import "../styles/utils/color-palette.scss";
 
-  .fbx-panel-radio-button__inner-content {
-    padding: 20px;
-  }
+.fbx-panel-radio-button {
+  display: block;
+  border: 1px solid $medium-gray;
+  background-color: $white;
 }
 
+.radio-input--hidden {
+  position: absolute;
+  opacity: 0;
+}
+
+.panel-radio-button__inner-content {
+}
+
+.panel-radio-button__header {
+  display: flex;
+  align-items: center;
+  padding: 5px 12px;
+  @include font(18);
+  background-color: $extra-light-gray;
+}
+
+.panel-radio-button__circle-indicator {
+  flex: 0 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  margin-right: 11px;
+  border-radius: 50%;
+  border: 1px solid $extra-dark-gray;
+}
+
+.selected-radio-circle {
+  display: none;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: $dark-green;
+}
+
+.panel-radio-button__body {
+  padding: 10px 15px 15px;
+  @include font(14);
+  line-height: 23px;
+  color: $extra-dark-gray;
+}
 </style>

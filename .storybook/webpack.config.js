fe34586a665b4e3f68a014e7dd93e2201fcd551f
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-01 16:19:26 -0700

    Add html loader to webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
         ],
       },
       {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
         test: /\.scss$/,
         use: [
           "vue-style-loader",

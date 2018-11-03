a8fe9d48c0bf8e9d34426a32071e6a1c1c39b2e3
Eyal Ella <eyal.ella@fundbox.com>
2018-11-03 22:08:28 +0200

    Added FbxAutofocus directive Added autofocus directive and implemented it inside FbxTextField

--- main.stories.js
+++ main.stories.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 
+// Components
 import IntroductionStories from './components/introduction/introduction.stories.js';
 import FbxButtonStories from './components/FbxButton/FbxButton.stories.js';
 import FbxCheckboxStories from './components/FbxCheckbox/FbxCheckbox.stories.js';
@@ -7,3 +8,6 @@ import FbxCloseButtonStories from './components/FbxCloseButton/FbxCloseButton.st
 import FbxTabs from './components/FbxTabs/FbxTabs.stories.js';
 import FbxTextField from './components/FbxTextField/FbxTextField.stories.js';
 import FbxTooltip from './components/FbxTooltip/FbxTooltip.stories.js';
+
+// Directives
+import FbxAutofocus from "./directives/FbxAutofocus/FbxAutofocus.stories.js";

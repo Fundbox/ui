1585b421459fe3bc2b01e2018caaca3c55a41a05
Eyal Ella <eyal.ella@fundbox.com>
2018-11-03 11:26:28 +0200

    Nest Storybook components under a components namespace In the future we will have directives as well as components and maybe more. To not have everything in one long list in story book I nested the components under a components namespace. Also I moved all common stuff like styles and assets to the project root

---
+++ main.stories.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+import IntroductionStories from './components/introduction/introduction.stories.js';
+import FbxButtonStories from './components/FbxButton/FbxButton.stories.js';
+import FbxCheckboxStories from './components/FbxCheckbox/FbxCheckbox.stories.js';
+import FbxCloseButtonStories from './components/FbxCloseButton/FbxCloseButton.stories.js';
+import FbxTabs from './components/FbxTabs/FbxTabs.stories.js';
+import FbxTextField from './components/FbxTextField/FbxTextField.stories.js';
+import FbxTooltip from './components/FbxTooltip/FbxTooltip.stories.js';

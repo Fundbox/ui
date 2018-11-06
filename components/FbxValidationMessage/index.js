0e368c3b4899301140404bdbb4126f1e86504450
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-06 10:33:29 -0800

    Add index file for FbxValidationMessage

---
+++ components/FbxValidationMessage/index.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import FbxValidationMessage from "./FbxValidationMessage.vue";
+
+Vue.component(FbxValidationMessage.name, FbxValidationMessage);
+
+export default FbxValidationMessage;

65c1a80004f79c58e9ae1280b91bb621270d94ec
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-07 09:31:57 -0800

    Reorder import

--- components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
+++ components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
@@ -1,11 +1,11 @@
 import { storiesOf } from '@storybook/vue';
 import { withInfo } from 'storybook-addon-vue-info';
 
+import './FbxPanelRadioButton.stories.scss';
 import summary from './FbxPanelRadioButton.md';
 import DefaultStoryComponent from "./FbxPanelRadioButton.Default.story.js";
 import NoIconsStoryComponent from './FbxPanelRadioButton.NoIcons.story.vue';
 import WithValidationStoryComponent from './FbxPanelRadioButton.WithValidation.story.vue';
-import './FbxPanelRadioButton.stories.scss';
 
 const stories = storiesOf('Components/PanelRadioButton', module);
 

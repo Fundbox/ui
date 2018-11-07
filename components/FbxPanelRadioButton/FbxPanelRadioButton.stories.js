e9c7e03bb1b39aaed12370bc6f48b7babb268c42
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-07 09:25:31 -0800

    Inline the default story template

--- components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
+++ components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
@@ -10,7 +10,6 @@ import summary from './FbxPanelRadioButton.md';
 import icon from '../../assets/logo.png';
 import icon2 from '../../assets/icon_check_active.svg';
 import icon3 from '../../assets/icon_check_disabled.svg';
-import storyHTML from './FbxPanelRadioButton.stories.html';
 import NoIconsStoryComponent from './FbxPanelRadioButton.NoIcons.story.vue';
 import validationStoryHTML from './FbxPanelRadioButton-validation.stories.html';
 import './FbxPanelRadioButton.stories.scss';
@@ -29,7 +28,47 @@ stories.add('default', withInfo({ summary })(() => ({
       bodyText: text('Text', 'Halvah chupa chups caramels sugar sugar plum ice cream wafer sugar pudding fruitcake snaps donut. lemon drops topping marshmallow sesame gummi bears sugar. biscuit dessert powder croissant jujubes powder chocolate tootsie topping.')
     }
   },
-  template: storyHTML,
+  template: `
+          <div class="fbx-panel-group-wrapper">
+            <div>Selected: {{ selectedDataSource }}</div>
+
+            <div class="fbx-panel-radio-button-group">
+              <fbx-panel-radio-button
+                :iconPath="icon"
+                v-model="selectedDataSource"
+                :value="headerText"
+                @change="onCoolChange"
+              >
+                <div slot="header">{{ headerText }}</div>
+                <div slot="body">{{ bodyText }}</div>
+              </fbx-panel-radio-button>
+
+              <fbx-panel-radio-button
+                :iconPath="icon2"
+                v-model="selectedDataSource"
+                value="OPTION TWO"
+                @change="onCoolChange"
+              >
+                <div slot="header">Option 2</div>
+                <div slot="body">
+                  Dynamic HTML content <strong>is very handy</strong>
+                  <br />
+                  <pre>npm i -g npm</pre>
+                </div>
+              </fbx-panel-radio-button>
+
+              <fbx-panel-radio-button
+                :iconPath="icon3"
+                v-model="selectedDataSource"
+                value="option the third"
+                @change="onCoolChange"
+              >
+                <div slot="header">Option 3</div>
+                <div slot="body">This option is the best</div>
+              </fbx-panel-radio-button>
+            </div>
+          </div>
+  `,
   methods: {
     onCoolChange: action('@change')
   },

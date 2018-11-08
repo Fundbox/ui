6fd709d880d6106adb286b338418a62e2bf84f78
Nicholas Litwin <nick.litwin@fundbox.com>
2018-11-08 11:38:47 -0800

    Extract out default story

--- components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
+++ components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js
@@ -3,12 +3,13 @@ import { withInfo } from 'storybook-addon-vue-info';
 
 import './FbxPanelRadioButton.stories.scss';
 import summary from './FbxPanelRadioButton.md';
-import DefaultStoryComponent from "./FbxPanelRadioButton.Default.story.js";
+import DynamicValuesStoryComponent from "./FbxPanelRadioButton.DynamicValues.story.vue";
 import NoIconsStoryComponent from './FbxPanelRadioButton.NoIcons.story.vue';
 import WithValidationStoryComponent from './FbxPanelRadioButton.WithValidation.story.vue';
 
 const stories = storiesOf('Components/PanelRadioButton', module);
 
 stories.add('default', withInfo({ summary })(() => DefaultStoryComponent));
+stories.add('dynamic text and icons', () => DynamicValuesStoryComponent);
 stories.add('without icons', () => NoIconsStoryComponent);
 stories.add('with validation', () => WithValidationStoryComponent);

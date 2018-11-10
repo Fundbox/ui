f0b28c9ab1690affefdf9a8af22fba46ecf1474a
forforeach <forforeach@gmail.com>
2018-11-10 21:28:42 +0200

    feat(infra): update babel config with production env entry

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,38 @@
 module.exports = {
-  "env": {
-    "test": {
-      "presets": [
+  env: {
+    production: {
+      presets: [
         "@babel/preset-env"
       ],
-      "plugins": [
+      plugins: [
+        "@babel/plugin-transform-runtime",
         [
           "@babel/plugin-proposal-object-rest-spread",
           {
-            "useBuiltIns": true
+            useBuiltIns: true
+          }
+        ],
+        [
+          "@babel/plugin-proposal-decorators",
+          {
+            legacy: true
+          }
+        ],
+        "@babel/plugin-proposal-function-sent",
+        "@babel/plugin-proposal-export-namespace-from",
+        "@babel/plugin-proposal-numeric-separator",
+        "@babel/plugin-proposal-throw-expressions"
+      ],
+    },
+    test: {
+      presets: [
+        "@babel/preset-env"
+      ],
+      plugins: [
+        [
+          "@babel/plugin-proposal-object-rest-spread",
+          {
+            useBuiltIns: true
           }
         ]
       ]

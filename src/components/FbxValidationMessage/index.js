5948f5250afc383cedce3d94317feb46a4297640
forforeach <forforeach@gmail.com>
2018-11-10 21:32:50 +0200

    feat(components): add missing plugin registration for component

---
+++ src/components/FbxValidationMessage/index.js
@@ -0,0 +1,16 @@
+import FbxValidationMessage from './FbxValidationMessage.vue'
+import { registerComponents, vueUse } from '../../utils/plugins'
+
+const components = {
+  FbxValidationMessage
+}
+
+const VuePlugin = {
+  install (Vue) {
+    registerComponents(Vue, components)
+  }
+}
+
+vueUse(VuePlugin)
+
+export default VuePlugin

70fb3f2cbfaee04bdb8ea04ca2351b66348ed285
liran <liran.hershkovich@fundbox.com>
2018-12-12 16:59:41 +0200

    added unit tests

--- src/components/FbxModalWrapper/FbxModalWrapper.spec.js
+++ src/components/FbxModalWrapper/FbxModalWrapper.spec.js
@@ -1,11 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import FbxModalWrapper from './FbxModalWrapper.vue'
+import VeeValidate from 'vee-validate'
+
+const v = new VeeValidate.Validator()
 
 describe('Components/FbxModalWrapper', () => {
   describe('snapshots', () => {
     it('renders the default correctly', () => {
       const wrapper = shallowMount(FbxModalWrapper, {
-        // mounting options https://vue-test-utils.vuejs.org/api/options.html
+        propsData: {
+          title: 'snapshot title'
+        },
+        provide: () => ({
+          $validator: v
+        })
       })
       expect(wrapper.html()).toMatchSnapshot()
     })
@@ -13,17 +21,22 @@ describe('Components/FbxModalWrapper', () => {
     it('renders with loading correctly', () => {
       const wrapper = shallowMount(FbxModalWrapper, {
         propsData: {
-          isLoading: true
-        }
+          isLoading: true,
+          title: 'snapshot title'
+        },
+        provide: () => ({
+          $validator: v
+        })
       })
       expect(wrapper.html()).toMatchSnapshot()
     })
 
     it('renders without title correctly', () => {
       const wrapper = shallowMount(FbxModalWrapper, {
-        propsData: {
-          title: undefined
-        }
+        // mounting options https://vue-test-utils.vuejs.org/api/options.html
+        provide: () => ({
+          $validator: v
+        })
       })
       expect(wrapper.html()).toMatchSnapshot()
     })

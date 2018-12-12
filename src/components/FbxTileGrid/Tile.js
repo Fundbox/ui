f01c9ef5e735013cbb40119209bbacf7cef37f53
ItamarGronich <itamargronich@gmail.com>
2018-12-12 16:00:53 +0200

    Modify tile

--- src/components/FbxTileGrid/Tile.js
+++ src/components/FbxTileGrid/Tile.js
@@ -1,15 +1,10 @@
-function defineReadOnlyProperties(context, props) {
-  for (let prop in props) {
-    if (props.hasOwnProperty(prop)) {
-      Object.defineProperty(context, prop, { value: props[prop], writable: false })
-    }
-  }
-}
-
-
 export class Tile {
   constructor(name, imgNormal, imgHover, imgDisabled, hoverColor = '#2c3e50', isEnabled = true, isConnected = false) {
-    defineReadOnlyProperties(this, { name, imgNormal, imgHover, imgDisabled, hoverColor })
+    this.name = name
+    this.imgNormal = imgNormal
+    this.imgHover = imgHover
+    this.imgDisabled = imgDisabled
+    this.hoverColor = hoverColor
     this.isEnabled = isEnabled
     this.isConnected = isConnected
   }

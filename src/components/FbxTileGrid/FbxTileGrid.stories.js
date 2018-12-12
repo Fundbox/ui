2a4b156831a163ad40cc4faed741eb1ca5da2ec9
ItamarGronich <itamargronich@gmail.com>
2018-12-12 16:01:08 +0200

    edit story to match.

--- src/components/FbxTileGrid/FbxTileGrid.stories.js
+++ src/components/FbxTileGrid/FbxTileGrid.stories.js
@@ -1,12 +1,9 @@
 import { Tile } from './Tile'
 import { storiesOf } from '@storybook/vue'
 import { withInfo } from 'storybook-addon-vue-info'
-import { number as knobNumber, boolean as knobBoolean } from '@storybook/addon-knobs'
+import { number as numberKnob, boolean as booleanKnob, object as objectKnob } from '@storybook/addon-knobs'
 import FbxTileGrid from './FbxTileGrid.vue'
 import summary from './FbxTileGrid.md'
-
-const stories = storiesOf('Components/TileGrid', module)
-const withSummary = withInfo({ summary })
 
 import img5Normal from './story-assets/5_colored.svg'
 import img5Hover from './story-assets/5_white.svg'
@@ -36,34 +33,38 @@ import img2162Normal from './story-assets/2162_colored.svg'
 import img2162Hover from './story-assets/2162_white.svg'
 import img2162Disabled from './story-assets/2162_greyed.svg'
 
-const bankTilesCollection = [
-  new Tile('WellsFargo', img5Normal, img5Hover, img5Disabled, 'red'),
+const stories = storiesOf('Components/TileGrid', module)
+const withSummary = withInfo({ summary })
+
+const tileData = [
+  new Tile('WellsFargo', img5Normal, img5Hover, img5Disabled),
   new Tile('UnionBank', img459Normal, img459Hover, img459Disabled),
   new Tile('UsBank', img524Normal, img524Hover, img524Disabled),
-  new Tile('Chase', img643Normal, img643Hover, img643Disabled, '#5479ca', true, true),
-  new Tile('FifthThirdBank', img745Normal, img745Hover, img745Disabled, undefined, false),
+  new Tile('Chase', img643Normal, img643Hover, img643Disabled),
+  new Tile('FifthThirdBank', img745Normal, img745Hover, img745Disabled),
   new Tile('Huntington', img782Normal, img782Hover, img782Disabled),
   new Tile('Regions', img783Normal, img783Hover, img783Disabled),
   new Tile('CitiBank', img1603Normal, img1603Hover, img1603Disabled),
   new Tile('PncBank', img2162Normal, img2162Hover, img2162Disabled),
 ]
 
-
 function defaultStory() {
   return {
     data() {
+      const numberOfTilesKnob = numberKnob('Number of tiles', tileData.length)
+      const tileDataKnobed = tileData.map(tile => objectKnob(tile.name, tile))
       return {
-        tileData: bankTilesCollection.slice(0, knobNumber('Number of tiles', bankTilesCollection.length))
+        tileData: tileDataKnobed.slice(0, numberOfTilesKnob),
       }
     },
     components: { FbxTileGrid },
     template: `
       <div style="background: #f7f8f8; padding: 1rem">
-        <fbx-tile-grid :columns="${ knobNumber('Columns', 3) }" 
+        <fbx-tile-grid :columns="${ numberKnob('Columns', 3) }" 
                        :tile-data="tileData"
-                       :should-show-placeholders="${knobBoolean('Show placeholders', true)}"
+                       :should-show-placeholders="${ booleanKnob('Show placeholders', false) }"
                        >
-        </fbx-tile-grid> 
+        </fbx-tile-grid>
       </div>
     `,
   }

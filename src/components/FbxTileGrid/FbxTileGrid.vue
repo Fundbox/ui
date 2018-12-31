17924774c533ec550e8037b2abac7216d5c2b6fa
ItamarGronich <itamargronich@gmail.com>
2018-12-31 11:10:44 +0200

    Implement Tile component in grid.

--- src/components/FbxTileGrid/FbxTileGrid.vue
+++ src/components/FbxTileGrid/FbxTileGrid.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="container">
-    <div class="tile"
-         v-for="tile in tiles"
-         :key="tile.name"
-         :class="tileClasses(tile)"
-         :style="{ width: (100 / columns) + '%' }"
-         @mouseenter="onHoverOverTile($event, tile)"
-         @mouseleave="onHoverOverTile($event, tile)"
-         @click="onClick(tile)"
+    <div class="tile-wrapper"
+      v-for="tile in tiles"
+      :style="{ width: (100 / columns) + '%' }"
+      :key="tile.name"
     >
-      <div class="tile-inner" >
-        <template v-if="isTile(tile)">
-          <img class="normal" :src="tile.imgNormal" :alt="tile.name" v-show="tile.isEnabled" />
-          <img class="hover" :src="tile.imgHover" :alt="tile.name" />
-          <img class="disabled" :src="tile.imgDisabled" :alt="tile.name" v-show="!tile.isEnabled" />
-        </template>
-      </div>
+      <fbx-tile
+        :tile="tile"
+        @click="onClick(tile)"
+      >
+      </fbx-tile>
     </div>
   </div>
 </template>
 
 <script>
 import { range } from 'lodash'
-import { Tile } from './Tile'
 import { noop } from '../../utils'
+import FbxTile from '../FbxTile/FbxTile'
+import { Tile } from './Tile'
 
 export default {
   name: 'FbxTileGrid',
+  components: { FbxTile },
   props: {
     tileData: {
       type: Array,
@@ -51,28 +47,6 @@ export default {
       default() {
         return noop()
       },
-    },
-  },
-  methods: {
-    tileClasses(tile) {
-      return {
-        placeholderTile: !this.isTile(tile),
-        disabled: !tile.isEnabled,
-        connected: tile.isConnected,
-      }
-    },
-    isTile(tile) {
-      return tile instanceof Tile
-    },
-    onHoverOverTile(hoverEvent, tile) {
-      if (tile.isEnabled) {
-        let tileInner = hoverEvent.target.children[0]
-        if (hoverEvent.type === 'mouseenter') {
-          tileInner.style.background = tile.hoverColor
-        } else {
-          tileInner.style.background = ''
-        }
-      }
     },
   },
   computed: {
@@ -105,72 +79,11 @@ export default {
     flex-wrap: wrap;
   }
 
-  .tile {
+  .tile-wrapper {
     padding: 0 $horizontal-spacing $vertical-spacing 0;
-    position: relative;
     height: $height;
-    cursor: pointer;
+    position: relative;
     flex-shrink: 0;
     flex-grow: 0;
-
-    &.placeholderTile {
-      .tile-inner {
-        border: 2px solid $white;
-        background: transparent;
-      }
-    }
-
-    &-inner {
-      background: $white;
-      width: 100%;
-      height: 100%;
-      transition: background-color 200ms ease-in-out;
-      position:  relative;
-    }
-
-    img {
-      transition: opacity 200ms ease-in-out;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      max-width: 63%;
-      max-height: 55%;
-    }
-
-    .hover {
-      opacity: 0;
-    }
-
-    &:hover {
-      .hover {
-        opacity: 1;
-      }
-      .normal {
-        opacity: 0;
-      }
-    }
-
-    &.disabled {
-      cursor: not-allowed;
-    }
-
-    &.connected {
-      .tile-inner {
-        border: 2px solid $dark-green;
-
-        // Connected state check icon. Visual only.
-        &:after {
-          content: "";
-          position: absolute;
-          top: 0;
-          right: 0;
-          display: block;
-          width: 15px;
-          height: 15px;
-          background: url("../../assets/icon-check-white.svg") $dark-green no-repeat 65% 40%;
-        }
-      }
-    }
   }
 </style>

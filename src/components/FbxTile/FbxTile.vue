7f5b02f984d48a6fe3c7c415df62c457c9f4c5c8
ItamarGronich <itamargronich@gmail.com>
2018-12-31 09:35:33 +0200

    Implement Tile component.

--- src/components/FbxTile/FbxTile.vue
+++ src/components/FbxTile/FbxTile.vue
@@ -1,12 +1,113 @@
 <template>
-  <div>Hello from FbxTile!</div>
+  <div class="tile"
+       :class="tileClasses(tile)"
+       @mouseenter="onHoverOverTile($event, tile)"
+       @mouseleave="onHoverOverTile($event, tile)"
+  >
+    <template v-if="isTile(tile)">
+      <img class="normal" :src="tile.imgNormal" :alt="tile.name" v-show="tile.isEnabled" />
+      <img class="hover" :src="tile.imgHover" :alt="tile.name" />
+      <img class="disabled" :src="tile.imgDisabled" :alt="tile.name" v-show="!tile.isEnabled" />
+    </template>
+  </div>
 </template>
 
 <script>
+import { Tile } from '../FbxTileGrid'
+
 export default {
-  name: 'FbxTile'
+  name: 'FbxTile',
+  props: {
+    tile: {
+      type: Tile,
+      required: true,
+      validator(tile) {
+        return tile instanceof Tile
+      },
+    },
+  },
+  methods: {
+    tileClasses(tile) {
+      return {
+        placeholderTile: !this.isTile(tile),
+        disabled: !tile.isEnabled,
+        connected: tile.isConnected,
+      }
+    },
+    isTile(tile) {
+      return tile instanceof Tile
+    },
+    onHoverOverTile(hoverEvent, tile) {
+      if (tile.isEnabled) {
+        let tileElement = hoverEvent.target
+        if (hoverEvent.type === 'mouseenter') {
+          tileElement.style.background = tile.hoverColor
+        } else {
+          tileElement.style.background = ''
+        }
+      }
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "../../styles/utils/color-palette";
+
+  .tile {
+    cursor: pointer;
+    background: $white;
+    width: 100%;
+    height: 100%;
+    transition: background-color 200ms ease-in-out;
+    position:  relative;
+
+    &.placeholderTile {
+      border: 2px solid $white;
+      background: transparent;
+    }
+
+    img {
+      transition: opacity 200ms ease-in-out;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      max-width: 63%;
+      max-height: 55%;
+    }
+
+    .hover {
+      opacity: 0;
+    }
+
+    &:hover {
+      .hover {
+        opacity: 1;
+      }
+      .normal {
+        opacity: 0;
+      }
+    }
+
+    &.disabled {
+      cursor: not-allowed;
+    }
+
+    &.connected {
+      border: 2px solid $dark-green;
+
+      // Connected state check icon. Visual only.
+      &:after {
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: block;
+        width: 15px;
+        height: 15px;
+        background: url("../../assets/icon-check-white.svg") $dark-green no-repeat 65% 40%;
+      }
+    }
+  }
 </style>

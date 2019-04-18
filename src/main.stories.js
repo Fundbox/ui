9b44c291e3b5c9d83bfd4b682132ca57e659f7d0
Nick Litwin <nicklitwin88@gmail.com>
2019-04-18 11:34:33 -0700

    refactor(stories): organize components

--- src/main.stories.js
+++ src/main.stories.js
@@ -1,22 +1,29 @@
 /* eslint-disable no-unused-vars */
+import IntroductionStories from './components/introduction/introduction.stories.js'
 
 // Icons
 import Icomoon from './icons/Icomoon/Icomoon.stories.js'
 
 // Components
+// Alerts and Notifications
 import FbxNotice from './components/FbxNotice/FbxNotice.stories.js'
-import FbxIosSwitchButton from './components/FbxIosSwitchButton/FbxIosSwitchButton.stories.js'
+import FbxModalWrapper from './components/FbxModalWrapper/FbxModalWrapper.stories.js'
+import FbxTooltip from './components/FbxTooltip/FbxTooltip.stories.js'
+
+// Navigation
+import FbxTabs from './components/FbxTabs/FbxTabs.stories.js'
+
+// Layout
 import FbxTileGrid from './components/FbxTileGrid/FbxTileGrid.stories.js'
 import FbxTile from './components/FbxTile/FbxTile.stories.js'
-import FbxModalWrapper from './components/FbxModalWrapper/FbxModalWrapper.stories.js'
-import IntroductionStories from './components/introduction/introduction.stories.js'
+
+// Form Elements
 import FbxButtonStories from './components/FbxButton/FbxButton.stories.js'
 import FbxCheckboxStories from './components/FbxCheckbox/FbxCheckbox.stories.js'
 import FbxCloseButtonStories from './components/FbxCloseButton/FbxCloseButton.stories.js'
+import FbxIosSwitchButton from './components/FbxIosSwitchButton/FbxIosSwitchButton.stories.js'
 import FbxPanelRadioButtonStories from './components/FbxPanelRadioButton/FbxPanelRadioButton.stories.js'
-import FbxTabs from './components/FbxTabs/FbxTabs.stories.js'
 import FbxTextField from './components/FbxTextField/FbxTextField.stories.js'
-import FbxTooltip from './components/FbxTooltip/FbxTooltip.stories.js'
 import FbxSelect from './components/FbxSelect/FbxSelect.stories.js'
 
 // Directives

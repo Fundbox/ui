f362cb3b2df9137038ab5a7f048aab1f27975490
Nick Litwin <nicklitwin88@gmail.com>
2019-04-19 10:12:25 -0700

    test(FbxTextArea): add snapshot and unit tests

--- src/components/FbxTextArea/FbxTextArea.spec.js
+++ src/components/FbxTextArea/FbxTextArea.spec.js
@@ -5,8 +5,76 @@ describe('Components/FbxTextArea', () => {
   describe('snapshots', () => {
     it('renders the default correctly', () => {
       const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          name: 'my-text-area',
+          value: 'avocado',
+        },
       })
       expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders with a label', () => {
+      const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          name: 'my-text-area',
+          label: 'Order Details',
+        }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders with a resize value', () => {
+      const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          name: 'my-text-area',
+          resize: 'both',
+        }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders with a character counter', () => {
+      const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          name: 'my-text-area',
+          maxCount: 20,
+        }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('charCount', () => {
+    it('is 0 if there is no value length', () => {
+      const text = 'I am text'
+      const wrapper = shallowMount(FbxTextArea, {})
+
+      expect(wrapper.vm.charCount).toBe(0)
+    })
+
+    it('sets the current character count to the length of the value', () => {
+      const text = 'I am text'
+      const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          value: text,
+        },
+      })
+
+      expect(wrapper.vm.charCount).toBe(text.length)
+    })
+  })
+
+  describe('hasReachedMaxChars', () => {
+    it('returns true if the user has entered text that is too long', () => {
+      const text = 'I am text'
+      const wrapper = shallowMount(FbxTextArea, {
+        propsData: {
+          maxCount: text.length,
+          value: text,
+        },
+      })
+
+      expect(wrapper.vm.hasReachedMaxChars).toBe(true)
     })
   })
 })

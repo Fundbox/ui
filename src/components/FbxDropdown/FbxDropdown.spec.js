1b490fb03a51743a19365030c3a77b1751fa222a
ItamarGronich <itamargronich@gmail.com>
2019-04-23 14:22:34 +0300

    feature(DropDown): Add testing.

--- src/components/FbxDropdown/FbxDropdown.spec.js
+++ src/components/FbxDropdown/FbxDropdown.spec.js
@@ -4,10 +4,24 @@ import FbxDropdown from './FbxDropdown.vue'
 describe('Components/FbxDropdown', () => {
   describe('snapshots', () => {
     it('renders the default correctly', () => {
-      const wrapper = shallowMount(FbxDropdown, {
-        // mounting options https://vue-test-utils.vuejs.org/api/options.html
-      })
+      const wrapper = shallowMount(FbxDropdown, {})
       expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders the open menu', () => {
+      const wrapper = shallowMount(FbxDropdown, {})
+      wrapper.find('.drop-down-button').trigger('click')
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('menu closed when clicked outside',(done) => {
+      const wrapper = shallowMount(FbxDropdown, {})
+      wrapper.find('.drop-down-button').trigger('click')
+      wrapper.find('.drop-down-button').trigger('focusout')
+      setTimeout(() => {
+        expect(wrapper.html()).toMatchSnapshot()
+        done()
+      }, 500)
     })
   })
 })

b7a58406b4b57471b604ce9ad82e13ca7c7bbe2c
Nick Litwin <nicklitwin88@gmail.com>
2019-05-01 16:45:22 -0700

    test(FbxPrintLink): add unit and snapshot tests

--- src/components/FbxPrintLink/FbxPrintLink.spec.js
+++ src/components/FbxPrintLink/FbxPrintLink.spec.js
@@ -2,4 +2,68 @@ import { shallowMount } from '@vue/test-utils'
 import FbxPrintLink from './FbxPrintLink.vue'
 
 describe('Components/FbxPrintLink', () => {
+  const printTitle = "A title"
+  const printContent = "Some content"
+
+  describe('snapshots', () => {
+    it('renders the default correctly', () => {
+      const wrapper = shallowMount(FbxPrintLink, {
+        propsData: {
+          printTitle,
+          printContent,
+        }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders without a print icon', () => {
+      const wrapper = shallowMount(FbxPrintLink, {
+        propsData: {
+          printTitle,
+          printContent,
+          showPrintIcon: false,
+        }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('methodsprintTextContent', () => {
+    it('prints the new document', () => {
+      const focus = jest.fn()
+      const print = jest.fn()
+
+      window.open = jest.fn(() => {
+        return {
+          document: {
+            documentElement: {
+              innerHTML: "",
+            },
+          },
+          focus,
+          print,
+        }
+      })
+
+      const wrapper = shallowMount(FbxPrintLink, {
+        propsData: {
+          printTitle,
+          printContent,
+        },
+        mocks: {
+          $sanitize: text => text,
+        },
+      })
+
+      wrapper.vm.printTextContent()
+
+      const finalPrintContent = window.open.mock.results[0].value.document.documentElement.innerHTML
+      expect(finalPrintContent).toContain("DOCTYPE")
+      expect(finalPrintContent).toContain(printTitle)
+      expect(finalPrintContent).toContain(printContent)
+      expect(window.open).toHaveBeenCalledTimes(1)
+      expect(focus).toHaveBeenCalledTimes(1)
+      expect(print).toHaveBeenCalledTimes(1)
+    })
+  })
 })

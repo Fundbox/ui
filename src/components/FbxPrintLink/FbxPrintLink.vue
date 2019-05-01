55c39416c324e80374e02bc6549d9cd53ddbbb6d
Nick Litwin <nicklitwin88@gmail.com>
2019-05-01 16:44:54 -0700

    feat(FbxPrintLink): implement component

--- src/components/FbxPrintLink/FbxPrintLink.vue
+++ src/components/FbxPrintLink/FbxPrintLink.vue
@@ -1,13 +1,65 @@
 <template>
   <div class="fbx-print-link">
+    <div v-if="showPrintIcon" class="fbx-icon-printer"></div>
+    <a href="#" @click="printTextContent">Print this agreement</a>
   </div>
 </template>
 
  <script>
+import escape from 'lodash/escape'
+
 export default {
   name: 'FbxPrintLink',
+  props: {
+    printTitle: {
+      type: String,
+      required: true,
+    },
+    printContent: {
+      type: String,
+      required: true,
+    },
+    showPrintIcon: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    printTextContent() {
+      const printContent = `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>${escape(this.printTitle)}</title>
+          </head>
+          <body>
+            <h1>${escape(this.printTitle)}</h1>
+            <div>${this.$sanitize(this.printContent)}</div>
+          </body>
+        </html>
+      `
+      const newWindow = window.open()
+      newWindow.document.documentElement.innerHTML = printContent
+      newWindow.focus()
+      newWindow.print()
+    },
+  },
 }
 </script>
 
  <style lang="scss" scoped>
+@import "../../styles/utils/color-palette.scss";
+
+.fbx-print-link {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  color: $dark-green;
+}
+
+.fbx-icon-printer {
+  margin-right: 5px;
+}
 </style>

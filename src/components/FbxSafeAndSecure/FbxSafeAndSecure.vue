d97848f4415dc27f65860ce9e532861ec5b73738
ItamarGronich <itamargronich@gmail.com>
2019-05-02 21:14:01 +0300

    refactor(SafeAndSecure): change to better image

--- src/components/FbxSafeAndSecure/FbxSafeAndSecure.vue
+++ src/components/FbxSafeAndSecure/FbxSafeAndSecure.vue
@@ -1,55 +1,43 @@
 <template>
-  <article class="layout-row split vertical-center security-logos">
-      <a
-        class="security-logo security-logo-norton"
-        href="https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&amp;dn=fundbox.com&amp;lang=en"
-        target="_blank"
-      />
-      <a
-        class="security-logo security-logo-bbb"
-        target="_blank"
-        href="http://www.bbb.org/greater-san-francisco/business-reviews/financial-services/fundbox-in-san-francisco-ca-462656#bbbseal"
-        title="Fundbox, Inc. is a BBB Accredited Financial Service in San Francisco, CA"
-      />
-      <a
-        class="security-logo security-logo-mcafee"
-        href="https://www.mcafeesecure.com/verify?host=www.fundbox.com"
-        target="_blank"
-      />
+  <article class="safe-and-secure layout-row split vertical-center security-logos">
+      <a v-once v-for="img in images" :key="img.ref" class="security-logo" :href="img.ref" target="_blank">
+        <img class="security-logo-image" :src="img.src" alt="">
+      </a>
     </article>
 </template>
 
 <script>
+import norton from './assets/security-norton-logo.svg'
+import mcafee from './assets/security-mcafee-logo.svg'
+import bbb from './assets/security-bbb-logo.png'
+
 export default {
-  name: 'FbxSafeAndSecure'
+  name: 'FbxSafeAndSecure',
+  data() {
+    return {
+      images: [
+        { ref: 'https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&amp;dn=fundbox.com&amp;lang=en', src: norton },
+        { ref: 'http://www.bbb.org/greater-san-francisco/business-reviews/financial-services/fundbox-in-san-francisco-ca-462656#bbbseal', src: bbb },
+        { ref: 'https://www.mcafeesecure.com/verify?host=www.fundbox.com', src: mcafee },
+      ],
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../../styles/utils/utilities.scss";
 
-.security-logo {
+.safe-and-secure {
   height: 45px;
-  display: inline-block;
 }
 
-.security-logo-norton {
-  background: url("./assets/logo_norton.svg") no-repeat 50% 90%;
-  width: 85px;
-  margin-right: 25px;
+.security-logo {
+  position: relative;
+  flex: 0 0 auto;
 }
 
-.security-logo-bbb {
-  background: url("./assets/bbb@x2.png") left top no-repeat;
-  background-size: 179px 34px;
-  width: 89px;
-  height: 34px;
-  border-radius: 3px;
-  margin-right: 25px;
-}
-
-.security-logo-mcafee {
-  background: url("./assets/logo_mcafee.svg") no-repeat 50% 65%;
-  width: 82px;
+.security-logo-image, .security-logo {
+  height: 100%;
 }
 </style>
